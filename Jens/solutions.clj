f8591e34163551bf7c743d019cfb316c51458a85
Jens Axelsson <j.axelsson@gmail.com>
2018-12-02 17:22:15 +0100

    Day 2 part 2

--- Jens/solutions.clj
+++ Jens/solutions.clj
@@ -38,3 +38,20 @@
 	;; Sum all t:s and all d:s and multiply the results
 	(reduce #(vector (+ (first %1) (first %2)) (+ (second %1) (second %2))))
 	(reduce *))
+
+;;Day 2-2
+(loop
+	[remove 0]
+	(let
+		[current-strings (->>
+			;; Remove the nth character
+			(if (= 0 remove)
+				(map #(subs % 1) day2input)
+				(map #(str (subs % 0 remove) (subs % (+ 1 remove))) day2input))
+			;; Sort list by number of occurences of each string
+			frequencies
+			clojure.set/map-invert)]
+		;; Find duplicate and output it, or recur increasing remove by 1
+		(if (contains? current-strings 2)
+			(current-strings 2)
+			(recur (inc remove)))))
