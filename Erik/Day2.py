a6d19130654d5d69a685ec7f1d975555158c026f
Calluna <erik_jansson123@hotmail.com>
2018-12-02 12:35:46 +0100

    Day 2 part 2 py

--- Erik/Day2.py
+++ Erik/Day2.py
@@ -29,5 +29,33 @@ for m in range(0,len(mainList)):
 print("Found " +str(totalDoubles) +" doubles and " +str(totalTriples) +" triples. Checksum = " +str(totalDoubles * totalTriples))
 
 
-
 #Day 2 part 2
+
+def diffCounter(firstSearchStr,secondSearchStr):
+    diffs = 0
+    for i in range(0,len(firstSearchStr)):
+        if(firstSearchStr[i] != secondSearchStr[i]):
+            diffs += 1
+    return diffs
+
+doubleA = 0
+doubleB = 0
+for n in range(0,len(mainList)-1):
+    for m in range(0,len(mainList)-1):
+        if(n != m):
+            tempCounter = diffCounter(mainList[n],mainList[m])
+            if(tempCounter == 1):
+                doubleA = n
+                doubleB = m
+                break
+
+
+finalString = ""
+for i in range(0,len(mainList[doubleA])):
+    idOfDouble = 0
+    if((mainList[doubleA][i]) != (mainList[doubleB][i])):
+        idOfDouble = mainList[doubleA][i]
+        finalString = mainList[doubleA].replace(mainList[doubleA][i], "")
+
+
+print("Common characters in strings are: " +finalString)
