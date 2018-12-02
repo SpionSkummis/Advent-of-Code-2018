0e976977e7050218053762633b0e00fac5b16d83
Jens Axelsson <j.axelsson@gmail.com>
2018-12-02 15:19:09 +0100

    Day 2 part 1

--- Jens/solutions.clj
+++ Jens/solutions.clj
@@ -1,3 +1,5 @@
+;; DAY 1
+
 ;; Convert input to list of ints
 (def day1input (map read-string (clojure.string/split (slurp "./day1input.txt") #"\n")))
 
@@ -18,3 +20,21 @@
 				(map (fn [x] (some #(when (= x %) %) already-used)))
 				;; Grab the first one
 				(reduce #(if (not= nil %1) %1 %2))))))
+
+;; DAY 2
+
+;; Read input into list of strings
+(def day2input (clojure.string/split (slurp "./day2input.txt") #"\n"))
+
+;; Day 2-1
+(->> day2input
+	;; Sort strings alphabetically and split to lists of characters groups of 2 or more characters
+	(map #(apply str (sort %)))
+	(map (partial re-seq #"(.)\1+"))
+	(map (partial map first))
+	;; Create a list of vectors on the form [t d] where t and d are 1 or 0 depending on if the string contains triplets or duplicates, respectively
+	(map (partial map count))
+	(map #(vector (if (some (partial = 3) %) 1 0) (if (some (partial = 2) %) 1 0)))
+	;; Sum all t:s and all d:s and multiply the results
+	(reduce #(vector (+ (first %1) (first %2)) (+ (second %1) (second %2))))
+	(reduce *))
