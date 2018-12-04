e0c5d30a3ca9ae94bcc2aec85a73161458080d83
Jens Axelsson <j.axelsson@gmail.com>
2018-12-04 17:56:30 +0100

    Cleaned up formatting

--- Jens/day4.clj
+++ Jens/day4.clj
@@ -1,20 +1,21 @@
 ;; Reads input to a list of vectors on the form [month day hour minute event]
 (def day4input (mapv (partial drop 1) (map first (map (partial re-seq #"\d+\-(\d+)-(\d+)\s(\d+)\:(\d+)\]\s(.+)") (clojure.string/split (slurp "./day4input.txt") #"\n")))))
 
-;; Day 4-1
+;; Input formatted into vectors for each night: [elf-id 0 fell-asleep woke-up (etc.)]
 (def formatted-input (->> (sort-by (juxt first second #(nth % 2) #(nth % 3)) day4input)
-		;; If an id is present in the event string, it means a new night has started
-		(map #(if (not= 0 (count (re-find #"\d+" (nth % 4))))
-				;; New night, output [elf-id 0].
-				(vector (read-string (str "10r" (re-find #"\d+" (nth % 4)))) 0)
-				;; Same night, output [nil minutes-past-midnight]
-				(vector nil (read-string (str "10r" (nth % 3))))))
-		;; Reduce each night to one vector [elf-id 0 fell-asleep woke-up (etc.)]	
-		(reduce #(if (some? (first %2))
-			(conj %1 %2)
-			(conj (into [] (drop-last 1 %1)) (conj (last %1) (second %2))))
-			[])))
+	;; If an id is present in the event string, it means a new night has started
+	(map #(if (not= 0 (count (re-find #"\d+" (nth % 4))))
+		;; New night, output [elf-id 0].
+		(vector (read-string (str "10r" (re-find #"\d+" (nth % 4)))) 0)
+		;; Same night, output [nil minutes-past-midnight]
+		(vector nil (read-string (str "10r" (nth % 3))))))
+	;; Reduce each night to one vector [elf-id 0 fell-asleep woke-up (etc.)]	
+	(reduce #(if (some? (first %2))
+		(conj %1 %2)
+		(conj (into [] (drop-last 1 %1)) (conj (last %1) (second %2))))
+		[])))
 
+;; Day 4-1
 (let
 	[sleepiest (->> formatted-input
 		;; For each night, sum every wake-up timestamp and subtract every fall-asleep timestamp to get number of minutes slept per night
@@ -33,8 +34,7 @@
 		frequencies
 		(reduce #(if (> (second %1) (second %2)) %1 %2))
 		first)]
-	(* sleepiest most-slept-minute)
-)
+	(* sleepiest most-slept-minute))
 
 ;; Day 4-2
 (let [answer (->> formatted-input
@@ -47,7 +47,5 @@
 		;; Grab the first one and multiply elf-id by minute
 		last
 		first
-		(reduce *)
-	)]
-	answer
-)
+		(reduce *))]
+	answer)
