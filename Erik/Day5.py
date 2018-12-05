8f39185242f661d8764beb199c4188a52e1fb97f
Egendor <erik_jansson123@hotmail.com>
2018-12-05 14:43:00 +0100

    Minor day 5 fixes

--- Erik/Day5.py
+++ Erik/Day5.py
@@ -3,16 +3,14 @@ with open("./inputsE/input05.txt", "rt") as f:
 
 #testString = "dabAcCaCBAcCcaDA"
 
-def collapsePolymer(inList):
-    collapsedList = inList
+def collapsePolymer(collapsedList):
     for i in range((len(collapsedList) -1),0, -1):
         if((ord(collapsedList[i]) == (ord(collapsedList[i-1]) + 32)) or (ord(collapsedList[i]) == (ord(collapsedList[i-1]) - 32))):
             del collapsedList[i-1:i+1]
             i -= 1
     return collapsedList
 
-def removeChar(inList, charInt):
-    purgeList = inList # Bättre att göra nytt än att ändra i det som finns?
+def removeChar(purgeList, charInt):
     smallChar = chr(charInt+97)
     bigChar = chr(charInt+65)
     for i in range((len(purgeList) -1),-1, -1):
@@ -23,9 +21,6 @@ def removeChar(inList, charInt):
 print(len(collapsePolymer(inputList)))
 
 lenAfterPurge = [0]*26
-
-
 for i in range(0, len(lenAfterPurge)):
     lenAfterPurge[i] = len(collapsePolymer(removeChar(inputList.copy(),i)))
-
 print(min(lenAfterPurge))
