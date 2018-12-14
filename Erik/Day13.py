4814604150ace8ca9306450cdf20ff8f0f9cde22
Egendor <erik_jansson123@hotmail.com>
2018-12-14 02:15:33 +0100

    Day 13, not yet working, test inputs

--- Erik/Day13.py
+++ Erik/Day13.py
@@ -1,37 +1,138 @@
-with open("./inputsE/input13.txt", "rt") as f:
+with open("./inputsE/input13-t2.txt", "rt") as f:
     inputList = f.read().splitlines()
 
 print(len(inputList))
 print(len(inputList[0]))
 
+# ^^^^vvvv<>>>>>>>>   -- ska vara alla vagnar i min input
+
+def startDir(startChar):
+    if(startChar == "^"):
+        return  0
+    elif(startChar == ">"):
+        return  1
+    elif(startChar == "v"):
+        return  2
+    elif(startChar == "<"):
+        return  3
+    else:
+        print("Error in startDir function")
+
 class Cart:
-    def __init__(self,xPos,yPos,direction): #StartChar?
+    def __init__(self,xPos,yPos,startChar): #StartChar?
         self.xPos = xPos
         self.yPos = yPos
-        self.dir = direction # 0=N, 1=E, 2=S, 3=W.
+        self.dir = 8
+        self.dir = startDir(startChar) # 0=N, 1=E, 2=S, 3=W.
         self.turns = 0
 
-    def start_direction(self, startChar):
-        if(startChar == "^"):
-            self.dir = 0
-        elif(startChar == ">"):
-            self.dir = 1
-        elif(startChar == "v"):
-            self.dir = 2
-        else:
+#    def startDir(self, startChar):
+#        if(startChar == "^"):
+#            return  0
+#        elif(startChar == ">"):
+#            return  1
+#        elif(startChar == "v"):
+#            return  2
+#        else:
+#            return  3
             
 
-    def move
+    def move(self, mainMap):
+        posChar = mainMap[self.xPos][self.yPos]
+
+        if(posChar == "\\"):
+            if(self.dir == 0):
+                self.dir = 3
+            elif(self.dir == 1):
+                self.dir = 2
+            elif(self.dir == 2):
+                self.dir = 1
+            elif(self.dir == 3):
+                self.dir = 0
+
+        elif(posChar == "/"):
+            if(self.dir == 0):
+                self.dir = 1
+            elif(self.dir == 1):
+                self.dir = 0
+            elif(self.dir == 2):
+                self.dir = 3
+            elif(self.dir == 3):
+                self.dir = 2
+        
+        elif(posChar == "+"):
+            self.intersection_turn()
+
+        elif(posChar == " "):
+            print("Possible error at:",self.xPos,self.yPos)
+
+        if(self.dir == 0):
+            self.yPos += 1
+        elif(self.dir == 1):
+            self.xPos += 1
+        elif(self.dir == 2):
+            self.yPos -= 1
+        elif(self.dir == 3):
+            self.xPos -= 1
+
 
     def getpos(self):
-        return xPos, yPos
+        return self.xPos, self.yPos
 
     def intersection_turn(self):
         self.turnID = self.turns % 3
         if(self.turnID == 0):
             self.dir = (self.dir - 1)%4
         elif(self.turnID == 2):
-            self.dir 0 (self.dir + 1)%4
+            self.dir = (self.dir + 1)%4
         self.turns += 1
 
 
+def replaceCarts(rawMap):
+    replaceMap = [""]*len(rawMap)
+    """for x in range(0,len(rawMap)):
+        for y in range(0,len(rawMap[0])):
+            if((rawMap[x][y] == "<") or (rawMap[x][y] == ">")):
+                rawMap[x][y] = "-"
+            if((rawMap[x][y] == "^") or (rawMap[x][y] == "v")):
+                rawMap[x][y] = ("|")"""
+    for i in range(0,len(rawMap)):
+        step1 = rawMap[i].replace("<","-")
+        step2 = step1.replace(">","-")
+        step3 = step2.replace("^","|")
+        replaceMap[i] = step3.replace("v","|")
+    return replaceMap.copy()
+
+
+
+#Skapa en lista med alla vagnar
+wagonList = []
+for x in range(0,len(inputList)):
+    for y in range(0,len(inputList[0])):
+        if((inputList[x][y] == ">") or (inputList[x][y] == "<") or (inputList[x][y] == "v") or (inputList[x][y] == "^")):
+            wagonList.append(Cart(x,y,inputList[x][y]))
+            #print(inputList[x][y])
+#Konverterar vagnar till spår
+mainMap = replaceCarts(inputList)
+#print(len(wagonList))
+
+
+collision = False
+colXY = (0,0)
+
+tick = 0
+while(not collision):
+    print(tick)
+    tick += 1
+    for i in range(0,len(wagonList)):
+        wagonList[i].move(mainMap)
+        
+    """for i in range(0,len(wagonList)):
+        for j in range(i+1,len(wagonList)):
+            if(wagonList[i].getpos() == wagonList[j].getpos()):
+                collision = True
+                colXY = wagonList[i].getpos()
+                print("BOOM!")"""
+    
+
+print(tick)
