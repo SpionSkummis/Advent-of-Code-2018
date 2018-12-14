e4dcfbd00bdf2d084c98d7305f0e5ad975ba88b5
Egendor <erik_jansson123@hotmail.com>
2018-12-14 04:45:54 +0100

    Day13 part 2 faulty but working on test case

--- Erik/Day13.py
+++ Erik/Day13.py
@@ -1,10 +1,7 @@
-with open("./inputsE/input13-t2.txt", "rt") as f:
+with open("./inputsE/input13.txt", "rt") as f:
     inputList = f.read().splitlines()
 
-print(len(inputList))
-print(len(inputList[0]))
-
-# ^^^^vvvv<>>>>>>>>   -- ska vara alla vagnar i min input
+# ^^^^vvvv<>>>>>>>>   -- ska vara alla vagnar i min input 17st - 8 krockar
 
 def startDir(startChar):
     if(startChar == "^"):
@@ -23,7 +20,7 @@ class Cart:
         self.xPos = xPos
         self.yPos = yPos
         self.dir = 8
-        self.dir = startDir(startChar) # 0=N, 1=E, 2=S, 3=W.
+        self.dir = startDir(startChar) # 0=N, 1=E, 2=S, 3=W. Also x+ v, y+ ->
         self.turns = 0
 
 #    def startDir(self, startChar):
@@ -67,17 +64,17 @@ class Cart:
             print("Possible error at:",self.xPos,self.yPos)
 
         if(self.dir == 0):
-            self.yPos += 1
-        elif(self.dir == 1):
-            self.xPos += 1
-        elif(self.dir == 2):
-            self.yPos -= 1
-        elif(self.dir == 3):
             self.xPos -= 1
+        elif(self.dir == 1):
+            self.yPos += 1
+        elif(self.dir == 2):
+            self.xPos += 1
+        elif(self.dir == 3):
+            self.yPos -= 1
 
 
     def getpos(self):
-        return self.xPos, self.yPos
+        return self.yPos, self.xPos
 
     def intersection_turn(self):
         self.turnID = self.turns % 3
@@ -90,12 +87,6 @@ class Cart:
 
 def replaceCarts(rawMap):
     replaceMap = [""]*len(rawMap)
-    """for x in range(0,len(rawMap)):
-        for y in range(0,len(rawMap[0])):
-            if((rawMap[x][y] == "<") or (rawMap[x][y] == ">")):
-                rawMap[x][y] = "-"
-            if((rawMap[x][y] == "^") or (rawMap[x][y] == "v")):
-                rawMap[x][y] = ("|")"""
     for i in range(0,len(rawMap)):
         step1 = rawMap[i].replace("<","-")
         step2 = step1.replace(">","-")
@@ -111,28 +102,83 @@ for x in range(0,len(inputList)):
     for y in range(0,len(inputList[0])):
         if((inputList[x][y] == ">") or (inputList[x][y] == "<") or (inputList[x][y] == "v") or (inputList[x][y] == "^")):
             wagonList.append(Cart(x,y,inputList[x][y]))
-            #print(inputList[x][y])
+
 #Konverterar vagnar till spår
 mainMap = replaceCarts(inputList)
-#print(len(wagonList))
-
 
 collision = False
 colXY = (0,0)
-
 tick = 0
 while(not collision):
-    print(tick)
     tick += 1
     for i in range(0,len(wagonList)):
         wagonList[i].move(mainMap)
         
-    """for i in range(0,len(wagonList)):
+    for i in range(0,len(wagonList)):
         for j in range(i+1,len(wagonList)):
             if(wagonList[i].getpos() == wagonList[j].getpos()):
                 collision = True
                 colXY = wagonList[i].getpos()
-                print("BOOM!")"""
+                #print("BOOM!")
     
 
+print("After",tick,"rounds, the first colision was at", colXY)
+
+
+
+
+mainMap2 = replaceCarts(inputList)
+wagonList2 = []
+for x in range(0,len(inputList)):
+    for y in range(0,len(inputList[0])):
+        if((inputList[x][y] == ">") or (inputList[x][y] == "<") or (inputList[x][y] == "v") or (inputList[x][y] == "^")):
+            wagonList2.append(Cart(x,y,inputList[x][y]))
+
+
+tick = 0
+while(len(wagonList2) > 1):
+    tick += 1
+    collidedCarts = []
+
+    for i in range(0,len(wagonList2)):
+        wagonList2[i].move(mainMap2)
+
+    for i in range(0,len(wagonList2)):
+        for j in range(0,len(wagonList2)):
+            if((wagonList2[i].getpos() == wagonList2[j].getpos()) and (i != j)):
+                print("collision detected at", wagonList2[i].getpos(), wagonList2[j].getpos(), "tick", tick, i, j)
+                if(i not in collidedCarts):
+                    collidedCarts.append(i)
+                if(j not in collidedCarts):
+                    collidedCarts.append(j)
+        
+    collidedCarts.sort()
+    for i in range(len(collidedCarts)-1,-1,-1):
+        print(wagonList2[collidedCarts[i]].getpos(),tick,collidedCarts[i])
+        wagonList2.pop(collidedCarts[i])
+
+
+
+    
+    
+    """
+    if(len(collidedCarts) > 0):
+        print(collidedCarts.sort())
+
+        
+    collidedCarts.sort()
+    for i in range(len(collidedCarts)-1, 0, -1):
+        
+        print("BOOM!",wagonList2[collidedCarts[i]].getpos(), "tick", tick)
+        wagonList2.pop(collidedCarts[i])
+    """
+#wagonList2[0].move(mainMap2)
+print(wagonList2[0].getpos())
 print(tick)
+
+#print(colXY)
+
+
+
+
+#Test (73,134) - fel (87,11) - fel (86,11) - fel
