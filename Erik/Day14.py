463eb6dea0a685a1633ef979ab42a20bdce585e5
Egendor <erik_jansson123@hotmail.com>
2018-12-16 00:59:33 +0100

    Day14 part 2 working

--- Erik/Day14.py
+++ Erik/Day14.py
@@ -1,6 +1,8 @@
 inputNum = 380621
-#manuList = [3,8,6,2,1]
-manuList = [5,1,5,8,9]
+manuList = [3,8,0,6,2,1]
+#manuList = [5,1,5,8,9]
+#manuList = [9,2,5,1,0]
+#manuList = [5,9,4,1,4]
 
 
 
@@ -22,7 +24,7 @@ for i in range(0,recLimit+10):
 
 
 
-#print(cookList[-10:])
+print(cookList[-10:])
 print(cookList[(recLimit):(recLimit+10)])
 
 
@@ -33,25 +35,33 @@ cookList = [3,7]
 elf1Pos = 0
 elf2Pos = 1
 recLimit = inputNum
-
-
-for i in range(0,recLimit+10):
-
+lastAddIsDual = False
+patternFound = False
+while(not patternFound):
+#for i in range(0,recLimit+10):
     scoreSum = cookList[elf1Pos] + cookList[elf2Pos]
     if(scoreSum > 9):
         cookList.append(scoreSum//10)
         cookList.append(scoreSum%10)
+        if(cookList[-6:] == manuList or cookList[-7:-1] == manuList):
+            if(cookList[-7:-1] == manuList):
+                lastAddIsDual = True
+            print("hopp")
+            patternFound = True
     else:
         cookList.append(scoreSum)
+        if(cookList[-6:] == manuList):
+            patternFound = True
     elf1Pos = (elf1Pos +1 + cookList[elf1Pos]) % (len(cookList))
     elf2Pos = (elf2Pos +1 + cookList[elf2Pos]) % (len(cookList))
 
-    if(manuList in cookList): #This does not work.
-        print(i)
-        break
+    
+subtractNum = 6
+if(lastAddIsDual):
+    subtractNum =+ 1
+print(len(cookList)-subtractNum)
 
-
-
+#38052 - too low 20182291 - too high
 
 
 
