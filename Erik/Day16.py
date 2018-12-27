7fd26fe0ec6bf53e26f26d7e86ddcca8e8475001
Egendor <erik_jansson123@hotmail.com>
2018-12-27 22:12:10 +0100

    Day 16-1 working

--- Erik/Day16.py
+++ Erik/Day16.py
@@ -23,82 +23,98 @@ print(testList)
 
 
 # addr (add register) stores into register C the result of adding register A and register B.
-def addr(reg, instr):
+def addr(regIn, instr):
+    reg = regIn.copy()
     reg[instr[3]] = reg[instr[1]] + reg[instr[2]]
-    return reg
+    return reg.copy()
 # addi (add immediate) stores into register C the result of adding register A and value B.
-def addi(reg, instr):
+def addi(regIn, instr):
+    reg = regIn.copy()
     reg[instr[3]] = reg[instr[1]] + instr[2]
-    return reg
+    return reg.copy()
 # mulr (multiply register) stores into register C the result of multiplying register A and register B.
-def mulr(reg, instr):
+def mulr(regIn, instr):
+    reg = regIn.copy()
     reg[instr[3]] = reg[instr[1]] * reg[instr[2]]
-    return reg
+    return reg.copy()
 # muli (multiply immediate) stores into register C the result of multiplying register A and value B.
-def muli(reg, instr):
+def muli(regIn, instr):
+    reg = regIn.copy()
     reg[instr[3]] = reg[instr[1]] * instr[2]
     return reg
 # banr (bitwise AND register) stores into register C the result of the bitwise AND of register A and register B.
-def banr(reg, instr):
+def banr(regIn, instr):
+    reg = regIn.copy()
     reg[instr[3]] = reg[instr[1]] & reg[instr[2]]
     return reg
 # bani (bitwise AND immediate) stores into register C the result of the bitwise AND of register A and value B.
-def bani(reg, instr):
+def bani(regIn, instr):
+    reg = regIn.copy()
     reg[instr[3]] = reg[instr[1]] & instr[2]
     return reg
 # borr (bitwise OR register) stores into register C the result of the bitwise OR of register A and register B.
-def borr(reg, instr):
+def borr(regIn, instr):
+    reg = regIn.copy()
     reg[instr[3]] = reg[instr[1]] | reg[instr[2]]
     return reg
 # bori (bitwise OR immediate) stores into register C the result of the bitwise OR of register A and value B.
-def bori(reg, instr):
+def bori(regIn, instr):
+    reg = regIn.copy()
     reg[instr[3]] = reg[instr[1]] & instr[2]
     return reg
 # setr (set register) copies the contents of register A into register C. (Input B is ignored.)
-def setr(reg, instr):
+def setr(regIn, instr):
+    reg = regIn.copy()
     reg[instr[3]] = reg[instr[1]]
     return reg
 # seti (set immediate) stores value A into register C. (Input B is ignored.)
-def seti(reg, instr):
+def seti(regIn, instr):
+    reg = regIn.copy()
     reg[instr[3]] = instr[1]
     return reg
 # gtir (greater-than immediate/register) sets register C to 1 if value A is greater than register B. Otherwise, register C is set to 0.
-def gtir(reg, instr):
+def gtir(regIn, instr):
+    reg = regIn.copy()
     if(instr[1] > reg[instr[2]]):
         reg[instr[3]] = 1
     else:
         reg[instr[3]] = 0
     return reg
 # gtri (greater-than register/immediate) sets register C to 1 if register A is greater than value B. Otherwise, register C is set to 0.
-def gtri(reg, instr):
+def gtri(regIn, instr):
+    reg = regIn.copy()
     if(reg[instr[1]] > instr[2]):
         reg[instr[3]] = 1
     else:
         reg[instr[3]] = 0
     return reg
 # gtrr (greater-than register/register) sets register C to 1 if register A is greater than register B. Otherwise, register C is set to 0.
-def gtrr(reg, instr):
+def gtrr(regIn, instr):
+    reg = regIn.copy()
     if(reg[instr[1]] > reg[instr[2]]):
         reg[instr[3]] = 1
     else:
         reg[instr[3]] = 0
     return reg
 # eqir (equal immediate/register) sets register C to 1 if value A is equal to register B. Otherwise, register C is set to 0.
-def eqir(reg, instr):
+def eqir(regIn, instr):
+    reg = regIn.copy()
     if(instr[1] == reg[instr[2]]):
         reg[instr[3]] = 1
     else:
         reg[instr[3]] = 0
     return reg
 # eqri (equal register/immediate) sets register C to 1 if register A is equal to value B. Otherwise, register C is set to 0.
-def eqri(reg, instr):
+def eqri(regIn, instr):
+    reg = regIn.copy()
     if(reg[instr[1]] == instr[2]):
         reg[instr[3]] = 1
     else:
         reg[instr[3]] = 0
     return reg
 # eqrr (equal register/register) sets register C to 1 if register A is equal to register B. Otherwise, register C is set to 0.
-def eqrr(reg, instr):
+def eqrr(regIn, instr):
+    reg = regIn.copy()
     if(reg[instr[1]] == reg[instr[2]]):
         reg[instr[3]] = 1
     else:
@@ -111,4 +127,22 @@ def solveAll(startRegister, instructionSet):
     answList = [addr(r,i),addi(r,i),mulr(r,i),muli(r,i),banr(r,i),bani(r,i),borr(r,i),bori(r,i),setr(r,i),seti(r,i),gtir(r,i),gtri(r,i),gtrr(r,i),eqir(r,i),eqri(r,i),eqrr(r,i)]
     return answList
 
-print(solveAll(testList[0],testList[1]))
+def actsLike(register, instructions):
+    allCommands = ["addr","addi","mulr","muli","banr","bani","borr","bori","..."]
+    pass
+
+
+
+
+
+mainCount = 0
+for i in range(0,len(mainList)):
+    counter = 0
+    solvedList = solveAll(mainList[i][0],mainList[i][1])
+    for j in range(0,len(solvedList)):
+        if(solvedList[j] == mainList[i][2]):
+            counter += 1
+    if(counter >= 3):
+        mainCount += 1
+
+print(mainCount)
